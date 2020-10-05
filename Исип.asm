11d8340013135c8c925b674fcfa1ccfa5c328934
D1ssmay <70686432+D1ssmay@users.noreply.github.com>
2020-10-05 09:56:44 +0300

    Add files via upload

---
+++ Исип.asm
@@ -0,0 +1,17 @@
+mov 0000, r0
+out r0, p0
+mov 0000, r1
+out r1, p1
+mov FFFF, r2
+out r2, p2
+mov AE57, r3
+out r3, p3
+mov 566A, r3
+out r3, p4
+mov 3C3C, r3
+out r3, p5
+mov 1818, r2
+out r2, p6
+mov 0000, r2
+out r2, p7
+
